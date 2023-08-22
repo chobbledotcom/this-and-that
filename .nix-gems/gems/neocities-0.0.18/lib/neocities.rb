f894c3dca411fd0cf694800b14ba3e96d8326c23
Stefan <git@stefn.co.uk>
2023-08-22 03:02:05 +0100

    gitignore

--- .nix-gems/gems/neocities-0.0.18/lib/neocities.rb
+++
@@ -1,3 +0,0 @@
-require File.join(File.dirname(__FILE__), 'neocities', 'version')
-require File.join(File.dirname(__FILE__), 'neocities', 'client')
-require File.join(File.dirname(__FILE__), 'neocities', 'cli')
--- .nix-gems/gems/neocities-0.0.18/lib/neocities/version.rb
+++
@@ -1,3 +0,0 @@
-module Neocities
-  VERSION = '0.0.18'
-end
